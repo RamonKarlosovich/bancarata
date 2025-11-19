7859c2e86fe357beb711ba23bc30e5754ef873cf
RamonKarlosovich <ramon_karlosovich@hotmail.com>
2025-11-18 21:34:19 -0700

    Agrega cache-busting a SwaggerDocs

--- components/SwaggerDocs.tsx
+++ components/SwaggerDocs.tsx
@@ -4,10 +4,13 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 export function SwaggerDocs() {
+  // Cache-busting: siempre fuerza a recargar swagger.json
+  const swaggerUrl = `/swagger.json?v=${Date.now()}`;
+
   return (
     <div style={{ height: "100vh" }}>
       <SwaggerUI
-        url="/swagger.json"
+        url={swaggerUrl}
         docExpansion="none"
         defaultModelsExpandDepth={-1}
       />
